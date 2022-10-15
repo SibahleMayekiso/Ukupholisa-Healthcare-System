8a2b79ef13b0a8941f5923a16866dd2603efb0f0
SibahleMayekiso <mayekisosibahle@gmail.com>
2022-10-15 17:28:36 +0200

    CRUD Methods: Added Read methods for Provider Class

--- Ukupholisa Healthcare System/Data Access Layer/ProviderData.cs
+++ Ukupholisa Healthcare System/Data Access Layer/ProviderData.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,23 @@ namespace Ukupholisa_Healthcare_System.Data_Access_Layer
 {
     class ProviderData
     {
+        //Connection to a local Database that will be used by the application
+        SqlConnection conn = new SqlConnection(@"Data Source=.; Initial Catalog = UkupholisaHealthcareDB; Integrated Security = SSPI");
+        SqlCommand cmd;
+        string queryString;
+
+        //CRUD Operations and Methods
+        //Create Methods
+        //Read Methods
+        #region Read Methods
+        public SqlDataAdapter ReadAllProviders()
+        {
+            string query = @"SELECT * FROM Provider";
+            SqlDataAdapter adapter = new SqlDataAdapter(query, conn);
+            return adapter;
+        }
+        #endregion
+        //Update Methods
+        //Delete Methods
     }
 }
