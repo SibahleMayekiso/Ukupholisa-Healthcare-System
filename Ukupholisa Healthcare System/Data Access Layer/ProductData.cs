d585bfe837ef48b5fc63a7c643e1bb07d432007d
SibahleMayekiso <mayekisosibahle@gmail.com>
2022-10-15 17:21:31 +0200

    CRUD Methods: Added Read methods for Product Class

--- Ukupholisa Healthcare System/Data Access Layer/ProductData.cs
+++ Ukupholisa Healthcare System/Data Access Layer/ProductData.cs
@@ -1,12 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Ukupholisa_Healthcare_System.Business_Logic_Layer;
 
 namespace Ukupholisa_Healthcare_System.Data_Access_Layer
 {
     class ProductData
     {
+        //Connection to a local Database that will be used by the application
+        SqlConnection conn = new SqlConnection(@"Data Source=.; Initial Catalog = UkupholisaHealthcareDB; Integrated Security = SSPI");
+        SqlCommand cmd;
+        string queryString;
+
+        //CRUD Operations and Methods
+        //Create Methods
+        //Read Methods
+        #region Read Methods
+        public SqlDataAdapter ReadAllProducts()
+        {
+            string query = @"SELECT * FROM Product";
+            SqlDataAdapter adapter = new SqlDataAdapter(query, conn);
+            return adapter;
+        }
+        #endregion
+        //Update Methods
+        //Delete Methods
     }
 }
