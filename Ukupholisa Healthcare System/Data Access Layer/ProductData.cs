0fc2f71e5f124a4a6ef9753605a61393f9c97a29
SibahleMayekiso <mayekisosibahle@gmail.com>
2022-10-16 20:11:54 +0200

    CRUD Methods: Added Update methods for Product Class

--- Ukupholisa Healthcare System/Data Access Layer/ProductData.cs
+++ Ukupholisa Healthcare System/Data Access Layer/ProductData.cs
@@ -30,6 +30,57 @@ namespace Ukupholisa_Healthcare_System.Data_Access_Layer
         }
         #endregion
         //Update Methods
+        #region Update Methods
+        public string UpdateProductDetails(Product product)
+        {
+            string queryStateMessage = "";
+            List<SqlParameter> parameterList = new List<SqlParameter>();
+
+            try
+            {
+                //Parameters here
+                SqlParameter productID = new SqlParameter("@productID", SqlDbType.Int);
+                parameterList.Add(productID);
+                SqlParameter prodName = new SqlParameter("@productName", SqlDbType.VarChar);
+                parameterList.Add(prodName);
+                SqlParameter prodType = new SqlParameter("@prodType", SqlDbType.VarChar);
+                parameterList.Add(prodType);
+                SqlParameter maxDependents = new SqlParameter("@maxDependents", SqlDbType.NVarChar);
+                parameterList.Add(maxDependents);
+
+                //Setting Parameter values
+                productID.Value = product.ProdutID;
+                prodName.Value = product.ProductName;
+                prodType.Value = product.ProductType;
+                maxDependents.Value = product.MaxDependents;
+
+                //Adding Parameter to SqlCommand
+                parameterList.ForEach(x => { cmd.Parameters.Add(x); });
+
+                //Executing Query
+                queryString = string.Format(
+                    @"UPDATE Product
+                    SET ProductName = @productName, ProductType = @prodType, MaxDependents = @maxDependents
+                    WHERE ProductID = @productID"
+                    );
+                cmd = new SqlCommand(queryString, conn);
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                queryStateMessage = string.Format("An error occured:\n{0}", e.Message);
+
+            }
+            finally
+            {
+                queryStateMessage = string.Format("Update Successful");
+                conn.Close();
+            }
+            return queryStateMessage;
+        }
+
+        #endregion
         //Delete Methods
     }
 }
