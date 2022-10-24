8802614c7244f71f88dc0307cc86b3b154ed39fb
SibahleMayekiso <mayekisosibahle@gmail.com>
2022-10-24 15:52:26 +0200

    Client UI Update

--- Ukupholisa Healthcare System/Presentation Layer/ClientUI.Designer.cs
+++ Ukupholisa Healthcare System/Presentation Layer/ClientUI.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Ukupholisa_Healthcare_System
 {
-    partial class Form1
+    partial class frmClient
     {
         /// <summary>
         /// Required designer variable.
@@ -48,177 +48,232 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.gbxClientForm = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // CleintName2
             // 
             this.CleintName2.AutoSize = true;
-            this.CleintName2.Location = new System.Drawing.Point(68, 68);
+            this.CleintName2.Location = new System.Drawing.Point(605, 82);
+            this.CleintName2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.CleintName2.Name = "CleintName2";
-            this.CleintName2.Size = new System.Drawing.Size(64, 13);
+            this.CleintName2.Size = new System.Drawing.Size(80, 16);
             this.CleintName2.TabIndex = 0;
             this.CleintName2.Text = "Client Name";
             // 
             // CleintSurName2
             // 
             this.CleintSurName2.AutoSize = true;
-            this.CleintSurName2.Location = new System.Drawing.Point(68, 91);
+            this.CleintSurName2.Location = new System.Drawing.Point(605, 110);
+            this.CleintSurName2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.CleintSurName2.Name = "CleintSurName2";
-            this.CleintSurName2.Size = new System.Drawing.Size(78, 13);
+            this.CleintSurName2.Size = new System.Drawing.Size(97, 16);
             this.CleintSurName2.TabIndex = 1;
             this.CleintSurName2.Text = "Client Surname";
             // 
             // CleintNumber2
             // 
             this.CleintNumber2.AutoSize = true;
-            this.CleintNumber2.Location = new System.Drawing.Point(68, 119);
+            this.CleintNumber2.Location = new System.Drawing.Point(605, 144);
+            this.CleintNumber2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.CleintNumber2.Name = "CleintNumber2";
-            this.CleintNumber2.Size = new System.Drawing.Size(73, 13);
+            this.CleintNumber2.Size = new System.Drawing.Size(91, 16);
             this.CleintNumber2.TabIndex = 2;
             this.CleintNumber2.Text = "Client Number";
             // 
             // CleintCondition2
             // 
             this.CleintCondition2.AutoSize = true;
-            this.CleintCondition2.Location = new System.Drawing.Point(68, 145);
+            this.CleintCondition2.Location = new System.Drawing.Point(605, 176);
+            this.CleintCondition2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.CleintCondition2.Name = "CleintCondition2";
-            this.CleintCondition2.Size = new System.Drawing.Size(80, 13);
+            this.CleintCondition2.Size = new System.Drawing.Size(99, 16);
             this.CleintCondition2.TabIndex = 3;
             this.CleintCondition2.Text = "Client Condition";
             // 
             // ClientHouseNumber2
             // 
             this.ClientHouseNumber2.AutoSize = true;
-            this.ClientHouseNumber2.Location = new System.Drawing.Point(68, 169);
+            this.ClientHouseNumber2.Location = new System.Drawing.Point(605, 206);
+            this.ClientHouseNumber2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.ClientHouseNumber2.Name = "ClientHouseNumber2";
-            this.ClientHouseNumber2.Size = new System.Drawing.Size(107, 13);
+            this.ClientHouseNumber2.Size = new System.Drawing.Size(134, 16);
             this.ClientHouseNumber2.TabIndex = 4;
             this.ClientHouseNumber2.Text = "Client House Number";
             // 
             // ClientSuburb2
             // 
             this.ClientSuburb2.AutoSize = true;
-            this.ClientSuburb2.Location = new System.Drawing.Point(68, 220);
+            this.ClientSuburb2.Location = new System.Drawing.Point(605, 269);
+            this.ClientSuburb2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.ClientSuburb2.Name = "ClientSuburb2";
-            this.ClientSuburb2.Size = new System.Drawing.Size(70, 13);
+            this.ClientSuburb2.Size = new System.Drawing.Size(86, 16);
             this.ClientSuburb2.TabIndex = 5;
             this.ClientSuburb2.Text = "Client Suburb";
             // 
             // ClientCity2
             // 
             this.ClientCity2.AutoSize = true;
-            this.ClientCity2.Location = new System.Drawing.Point(68, 242);
+            this.ClientCity2.Location = new System.Drawing.Point(605, 296);
+            this.ClientCity2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.ClientCity2.Name = "ClientCity2";
-            this.ClientCity2.Size = new System.Drawing.Size(53, 13);
+            this.ClientCity2.Size = new System.Drawing.Size(65, 16);
             this.ClientCity2.TabIndex = 6;
             this.ClientCity2.Text = "Client City";
             // 
             // ClientStreetName2
             // 
             this.ClientStreetName2.AutoSize = true;
-            this.ClientStreetName2.Location = new System.Drawing.Point(68, 195);
+            this.ClientStreetName2.Location = new System.Drawing.Point(605, 238);
+            this.ClientStreetName2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.ClientStreetName2.Name = "ClientStreetName2";
-            this.ClientStreetName2.Size = new System.Drawing.Size(95, 13);
+            this.ClientStreetName2.Size = new System.Drawing.Size(118, 16);
             this.ClientStreetName2.TabIndex = 7;
             this.ClientStreetName2.Text = "Client Street Name";
             // 
             // ClientName
             // 
-            this.ClientName.Location = new System.Drawing.Point(153, 61);
+            this.ClientName.Location = new System.Drawing.Point(718, 73);
+            this.ClientName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.ClientName.Name = "ClientName";
-            this.ClientName.Size = new System.Drawing.Size(100, 20);
+            this.ClientName.Size = new System.Drawing.Size(132, 22);
             this.ClientName.TabIndex = 8;
             // 
             // ClientSurname
             // 
-            this.ClientSurname.Location = new System.Drawing.Point(153, 84);
+            this.ClientSurname.Location = new System.Drawing.Point(718, 101);
+            this.ClientSurname.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.ClientSurname.Name = "ClientSurname";
-            this.ClientSurname.Size = new System.Drawing.Size(100, 20);
+            this.ClientSurname.Size = new System.Drawing.Size(132, 22);
             this.ClientSurname.TabIndex = 9;
             // 
             // CleintNumber
             // 
-            this.CleintNumber.Location = new System.Drawing.Point(153, 116);
+            this.CleintNumber.Location = new System.Drawing.Point(718, 141);
+            this.CleintNumber.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.CleintNumber.Name = "CleintNumber";
-            this.CleintNumber.Size = new System.Drawing.Size(100, 20);
+            this.CleintNumber.Size = new System.Drawing.Size(132, 22);
             this.CleintNumber.TabIndex = 10;
             // 
             // ClientCondition
             // 
-            this.ClientCondition.Location = new System.Drawing.Point(153, 142);
+            this.ClientCondition.Location = new System.Drawing.Point(718, 173);
+            this.ClientCondition.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.ClientCondition.Name = "ClientCondition";
-            this.ClientCondition.Size = new System.Drawing.Size(100, 20);
+            this.ClientCondition.Size = new System.Drawing.Size(132, 22);
             this.ClientCondition.TabIndex = 11;
             // 
             // ClientHouseNumber
             // 
-            this.ClientHouseNumber.Location = new System.Drawing.Point(181, 166);
+            this.ClientHouseNumber.Location = new System.Drawing.Point(755, 202);
+            this.ClientHouseNumber.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.ClientHouseNumber.Name = "ClientHouseNumber";
-            this.ClientHouseNumber.Size = new System.Drawing.Size(100, 20);
+            this.ClientHouseNumber.Size = new System.Drawing.Size(132, 22);
             this.ClientHouseNumber.TabIndex = 12;
             // 
             // ClientStreetName
             // 
-            this.ClientStreetName.Location = new System.Drawing.Point(181, 192);
+            this.ClientStreetName.Location = new System.Drawing.Point(755, 234);
+            this.ClientStreetName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.ClientStreetName.Name = "ClientStreetName";
-            this.ClientStreetName.Size = new System.Drawing.Size(100, 20);
+            this.ClientStreetName.Size = new System.Drawing.Size(132, 22);
             this.ClientStreetName.TabIndex = 13;
             // 
             // ClientSuburb
             // 
-            this.ClientSuburb.Location = new System.Drawing.Point(181, 217);
+            this.ClientSuburb.Location = new System.Drawing.Point(755, 265);
+            this.ClientSuburb.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.ClientSuburb.Name = "ClientSuburb";
-            this.ClientSuburb.Size = new System.Drawing.Size(100, 20);
+            this.ClientSuburb.Size = new System.Drawing.Size(132, 22);
             this.ClientSuburb.TabIndex = 14;
             // 
             // ClientCity
             // 
-            this.ClientCity.Location = new System.Drawing.Point(181, 239);
+            this.ClientCity.Location = new System.Drawing.Point(755, 292);
+            this.ClientCity.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.ClientCity.Name = "ClientCity";
-            this.ClientCity.Size = new System.Drawing.Size(100, 20);
+            this.ClientCity.Size = new System.Drawing.Size(132, 22);
             this.ClientCity.TabIndex = 15;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(66, 279);
+            this.button1.Location = new System.Drawing.Point(431, 479);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(100, 28);
             this.button1.TabIndex = 16;
             this.button1.Text = "Call Centre";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(147, 279);
+            this.button2.Location = new System.Drawing.Point(539, 479);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(100, 28);
             this.button2.TabIndex = 17;
             this.button2.Text = "Database";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(228, 279);
+            this.button3.Location = new System.Drawing.Point(647, 479);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.Size = new System.Drawing.Size(100, 28);
             this.button3.TabIndex = 18;
             this.button3.Text = "Policy";
             this.button3.UseVisualStyleBackColor = true;
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(309, 279);
+            this.button4.Location = new System.Drawing.Point(755, 479);
+            this.button4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.Size = new System.Drawing.Size(100, 28);
             this.button4.TabIndex = 19;
             this.button4.Text = "Medical";
             this.button4.UseVisualStyleBackColor = true;
             // 
-            // Form1
+            // panel1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.panel1.BackColor = System.Drawing.SystemColors.Menu;
+            this.panel1.Location = new System.Drawing.Point(0, -2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 557);
+            this.panel1.TabIndex = 20;
+            // 
+            // gbxClientForm
+            // 
+            this.gbxClientForm.Location = new System.Drawing.Point(206, 12);
+            this.gbxClientForm.Name = "gbxClientForm";
+            this.gbxClientForm.Size = new System.Drawing.Size(246, 428);
+            this.gbxClientForm.TabIndex = 21;
+            this.gbxClientForm.TabStop = false;
+            this.gbxClientForm.Text = "Client Details";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(740, 322);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.TabIndex = 22;
+            // 
+            // frmClient
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.gbxClientForm);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
@@ -239,8 +294,10 @@
             this.Controls.Add(this.CleintNumber2);
             this.Controls.Add(this.CleintSurName2);
             this.Controls.Add(this.CleintName2);
-            this.Name = "Form1";
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "frmClient";
             this.Text = "Client";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- Ukupholisa Healthcare System/Presentation Layer/ClientUI.cs
+++ Ukupholisa Healthcare System/Presentation Layer/ClientUI.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Ukupholisa_Healthcare_System
 {
-    public partial class Form1 : Form
+    public partial class frmClient : Form
     {
-        public Form1()
+        public frmClient()
         {
             InitializeComponent();
         }
