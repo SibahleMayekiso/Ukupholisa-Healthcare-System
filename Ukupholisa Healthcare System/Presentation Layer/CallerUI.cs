94cf0ab504c2c7636ea4008244175210be5b8048
Christian <christianolivier43@gmail.com>
2022-11-07 09:33:34 +0200

    Timer Functionality

--- Ukupholisa Healthcare System/Presentation Layer/CallerUI.cs
+++ Ukupholisa Healthcare System/Presentation Layer/CallerUI.cs
@@ -10,25 +10,43 @@ using System.Windows.Forms;
 
 namespace Ukupholisa_Healthcare_System.Presentation_Layer
 {
-    public partial class frmCallerUI : Form
+    public partial class frmCallTimerUI : Form
     {
-        public frmCallerUI()
+        public bool end = false;
+        public frmCallTimerUI()
         {
             InitializeComponent();
         }
 
-        private void btnRejectCall_Click(object sender, EventArgs e)
+        private void btnEndCall_Click(object sender, EventArgs e)
         {
+            end = true;
+            frmCallerUI caller = new frmCallerUI();
+            caller.Close();
             Application.Exit();
+            
         }
 
-        private void btnAnswerCall_Click(object sender, EventArgs e)
+        private void frmCallTimerUI_Load(object sender, EventArgs e)
         {
-            frmCallCentreUI callCentreUI = new frmCallCentreUI();
-            frmCallTimerUI timer = new frmCallTimerUI();
-            callCentreUI.Show();
-            timer.Show();
-            this.Hide();
+            int i = 900;
+            while (i > 0)
+            {
+                string Sec = Convert.ToString(i % 60);
+                double min = i / 60;
+                string minutes = Convert.ToString(Math.Floor(min));
+                lblTime.Text = minutes + ":" + Sec;
+                if (i == 300)
+                {
+                    MessageBox.Show("Warning 5 minutes left");
+                }
+                if (end == true)
+                {
+                    i = 0;
+                }
+                
+                i = i - 1;
+            }
         }
     }
 }
