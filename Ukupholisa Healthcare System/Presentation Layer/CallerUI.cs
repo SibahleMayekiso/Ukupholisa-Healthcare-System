68e5934f73ac90c4205a7b483dbce8a8c7c84567
SibahleMayekiso <mayekisosibahle@gmail.com>
2022-11-07 09:48:24 +0200

    Revert "Timer Functionality"
    This reverts commit 94cf0ab504c2c7636ea4008244175210be5b8048.

--- Ukupholisa Healthcare System/Presentation Layer/CallerUI.cs
+++ Ukupholisa Healthcare System/Presentation Layer/CallerUI.cs
@@ -10,43 +10,25 @@ using System.Windows.Forms;
 
 namespace Ukupholisa_Healthcare_System.Presentation_Layer
 {
-    public partial class frmCallTimerUI : Form
+    public partial class frmCallerUI : Form
     {
-        public bool end = false;
-        public frmCallTimerUI()
+        public frmCallerUI()
         {
             InitializeComponent();
         }
 
-        private void btnEndCall_Click(object sender, EventArgs e)
+        private void btnRejectCall_Click(object sender, EventArgs e)
         {
-            end = true;
-            frmCallerUI caller = new frmCallerUI();
-            caller.Close();
             Application.Exit();
-            
         }
 
-        private void frmCallTimerUI_Load(object sender, EventArgs e)
+        private void btnAnswerCall_Click(object sender, EventArgs e)
         {
-            int i = 900;
-            while (i > 0)
-            {
-                string Sec = Convert.ToString(i % 60);
-                double min = i / 60;
-                string minutes = Convert.ToString(Math.Floor(min));
-                lblTime.Text = minutes + ":" + Sec;
-                if (i == 300)
-                {
-                    MessageBox.Show("Warning 5 minutes left");
-                }
-                if (end == true)
-                {
-                    i = 0;
-                }
-                
-                i = i - 1;
-            }
+            frmCallCentreUI callCentreUI = new frmCallCentreUI();
+            frmCallTimerUI timer = new frmCallTimerUI();
+            callCentreUI.Show();
+            timer.Show();
+            this.Hide();
         }
     }
 }
