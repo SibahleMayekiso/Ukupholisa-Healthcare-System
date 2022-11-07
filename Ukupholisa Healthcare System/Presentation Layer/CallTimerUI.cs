36ba79b5990c55e9614e2ce627fb20621a04769f
WastingToast151 <christianolivier43@gmail.com>
2022-11-07 10:05:24 +0200

    Timer Fixed

--- Ukupholisa Healthcare System/Presentation Layer/CallTimerUI.cs
+++ Ukupholisa Healthcare System/Presentation Layer/CallTimerUI.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,11 +7,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace Ukupholisa_Healthcare_System.Presentation_Layer
 {
     public partial class frmCallTimerUI : Form
     {
+        public bool end = false;
         public frmCallTimerUI()
         {
             InitializeComponent();
@@ -19,14 +21,33 @@ namespace Ukupholisa_Healthcare_System.Presentation_Layer
 
         private void btnEndCall_Click(object sender, EventArgs e)
         {
+            end = true;
             frmCallerUI caller = new frmCallerUI();
             caller.Close();
             Application.Exit();
+            
         }
 
         private void frmCallTimerUI_Load(object sender, EventArgs e)
         {
-
+            int i = 900;
+            while (i > 0)
+            {
+                string Sec = Convert.ToString(i % 60);
+                double min = i / 60;
+                string minutes = Convert.ToString(Math.Floor(min));
+                lblTime.Text = minutes + ":" + Sec;
+                if (i == 300)
+                {
+                    MessageBox.Show("Warning 5 minutes left");
+                }
+                if (end == true)
+                {
+                    i = 0;
+                }
+                Thread.Sleep(1000);
+                i = i - 1;
+            }
         }
     }
 }
