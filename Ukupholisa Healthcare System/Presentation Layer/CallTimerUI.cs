907dd0743b5f84ea786df367d54378dc65d6199a
SibahleMayekiso <mayekisosibahle@gmail.com>
2022-11-07 11:17:22 +0200

    Resolved Cross-Thread exception by making thread-safe call to Form control

--- Ukupholisa Healthcare System/Presentation Layer/CallTimerUI.cs
+++ Ukupholisa Healthcare System/Presentation Layer/CallTimerUI.cs
@@ -13,6 +13,25 @@ namespace Ukupholisa_Healthcare_System.Presentation_Layer
 {
     public partial class frmCallTimerUI : Form
     {
+        delegate void SetTextCallback(string text);
+        private void SetText(string text)
+        {
+            //Method is used to make a Thread-safe call to change the WinForm control property.
+
+            //**NOTE**: Multithreading can expose your code to very serious and complex bugs. Two or more
+            //threads manipulating a control can force the control into an inconsistent state
+            //and lead to race conditions, deadlocks, and freezes or hangs. If you implement
+            //multithreading in your app, be sure to call cross-thread controls in a thread-safe way.
+            if (lblTime.InvokeRequired)
+            {
+                SetTextCallback del = new SetTextCallback(SetText);
+                lblTime.Invoke(del, new object[] {text});
+            }
+            else
+            {
+                lblTime.Text = text;
+            }
+        }
         public void timer()
         {
             int i = 900;
@@ -21,7 +40,23 @@ namespace Ukupholisa_Healthcare_System.Presentation_Layer
                 string Sec = Convert.ToString(i % 60);
                 double min = i / 60;
                 string minutes = Convert.ToString(Math.Floor(min));
-                lblTime.Text = minutes + ":" + Sec;
+                //try
+                //{
+                //    //if (lblTime.InvokeRequired)
+                //    //{
+                //    //    lblTime.Invoke(new Action(timer));                                             
+                //    //    return;
+                //    //}
+                //    SetText
+                //    lblTime.Text = minutes + ":" + Sec;
+                //}
+                //catch (Exception e)
+                //{
+                //    MessageBox.Show(e.ToString());
+                //}
+
+                SetText(minutes + ":" + Sec);
+                
                 if (i == 300)
                 {
                     MessageBox.Show("Warning 5 minutes left");
@@ -32,7 +67,7 @@ namespace Ukupholisa_Healthcare_System.Presentation_Layer
                 }
                 Thread.Sleep(1000);
                 i = i - 1;
-            }
+            }           
         }
         public bool end = false;
         public frmCallTimerUI()
